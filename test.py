a293542094b97386ad45e526bb35c51029b66063
Carolina Johnson <csjohns@uw.edu>
2016-06-16 09:35:39 -0700

    changed test.py to using lists

--- test.py
+++ test.py
@@ -2,10 +2,12 @@ def print_hello_world():
 	print('hello world!')
 
 def fib(n):
+	fibs = []
 	a, b = 0, 1
 	for i in range(n):
 		a, b = b, a + b
-		yield a
+		fibs.append(a)
+	return fibs
 
 print(list(fib(10)))
 
